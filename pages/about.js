a2d59bad83a1a5c10284737f9cc552a758b9ec71
wabascript <rjwahba@gmail.com>
2022-02-24 17:33:38 -0500

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+export default function AboutPage(){
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the hottest DJ and other musical events near you</p>
+            <p>Version: 1.0.0</p>
+        </div>
+    )
+}
