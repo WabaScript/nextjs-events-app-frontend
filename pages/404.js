474bad3d728513f0090e8a5c771ff412603c1e5c
wabascript <rjwahba@gmail.com>
2022-02-25 13:30:40 -0500

    404 creation and code clean up with module aliases

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import {FaExclamationTriangle} from "react-icons/fa"
+import Layout from "@/components/Layout"
+import Link from "next/link"
+import styles from "@/styles/404.module.css"
+
+export default function NotFoundPage() {
+    return (
+        <Layout title='Page Not Found'>
+            <div className={styles.error}>
+                <h1><FaExclamationTriangle color="red"/><br/>404</h1>
+                <h4>Looks like you've wondered off!</h4>
+                <Link href='/'>Get back in there.</Link>
+            </div>
+
+        </Layout>
+    )
+}
