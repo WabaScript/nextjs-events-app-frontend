bb9809394d779f817502ba4d6cbbf099a18d4b43
wabascript <rjwahba@gmail.com>
2022-02-25 18:05:33 -0500

    testing fetch methods

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,14 +1,41 @@
 import { useRouter } from 'next/router'
 import Layout from '@/components/Layout'
+import {API_URL} from '@/config/index'
 
-export default function EventPage(){
+export default function EventPage({evt}){
     const router = useRouter()
 
     return (
         <Layout>
             <h1>My Event</h1>
-            <h3>{router.query.slug}</h3>
+            <h3>{evt.name}</h3>
             <button onClick={() => router.push('/')}>Click</button>
         </Layout>
     )
+}
+
+export async function getStaticPaths() {
+    const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json()
+
+    const paths = events.map(evt => ({
+        params: {slug: evt.slug}
+    }))
+
+    return {
+        paths,
+        fallback: true
+    }
+}
+
+export async function getStaticProps({params:{slug}}) {
+    const res = await fetch(`${API_URL}/api/events/${slug}`)
+    const events = await res.json()
+
+    return {
+        props: {
+            evt: events[0]
+        },
+        revalidate: 1
+    }
 }
